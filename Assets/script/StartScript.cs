88f0435652df6e223fe5123d528ba485c97fbe68
Darren K.J. Chen <kjchen@pm.me>
2021-01-02 03:33:50 +0800

    v20210102_COMMIT

--- Assets/script/StartScript.cs
+++ Assets/script/StartScript.cs
@@ -141,7 +141,6 @@ public class StartScript : MonoBehaviour
 
         int tmp = usr_fist_code - robot_fist_code;
 
-        //HandleDrawStamp drawStamp = GameObject.Find("draw_stamp").GetComponent<HandleDrawStamp>();
         if (tmp == 1 || tmp == -2) { usr_score++; updateScoreBoard(); Mibo.startTTS("恭喜你贏了，耶"); Mibo.motionPlay("666_SP_HorizontalBar"); }
         else if (tmp == 0) Mibo.startTTS("平手，看來我們不相上下呢"); // DRAW
         else { robot_score++; updateScoreBoard(); Mibo.startTTS("唉呀，你輸了，加把勁"); Mibo.motionPlay("666_PE_Phubbing"); }
